8fad7d05632f8aeb6e2984ecbb57257d56b99a9c
Sheets <smsheets@iastate.edu>
2019-11-05 17:50:53 -0600

    er update

--- ProjectDDL.sql
+++ ProjectDDL.sql
@@ -14,14 +14,23 @@ CREATE TABLE user
      followers    BIGINT, 
      following    BIGINT, 
      PRIMARY KEY(screen_name), 
-     FOREIGN KEY(location) REFERENCES State(name) 
+     FOREIGN KEY(location) REFERENCES state(name) 
   ); 
-  
-DROP TABLE IF EXISTS tweet;
 
-CREATE TABLE tweet
-  (
-	 id bigint,
-     text VARCHAR(280),
-     retweet_count BIGINT,
-  );
+DROP TABLE IF EXISTS tweet; 
+
+CREATE TABLE tweet 
+  ( 
+     id            BIGINT, 
+     created_at    DATE, 
+     day           VARCHAR(31), 
+     month         VARCHAR(12), 
+     year          BIGINT, 
+     text          VARCHAR(280), 
+     retweet_count BIGINT, 
+     retweeted     BIT, 
+     /* A bit is either 1, 0, or null. Good to use as a boolean. */ 
+     posting_user  VARCHAR(50), 
+     PRIMARY KEY(id), 
+     FOREIGN KEY(posting_user) REFERENCES user(screen_name) ON DELETE CASCADE 
+  ); 
