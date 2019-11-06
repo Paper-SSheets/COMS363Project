5589dfbd28448c85104f860fed2fd79bc85b9232
andmarek <andmarek@outlook.com>
2019-11-05 18:27:18 -0600

    added hashtag, state, and tagged

--- ProjectDDL.sql
+++ ProjectDDL.sql
@@ -1,36 +1,65 @@
-CREATE DATABASE IF NOT EXISTS 'group6'; 
+CREATE DATABASE IF NOT EXISTS 'group6';
 
-USE group6; 
+USE group6;
 
-DROP TABLE IF EXISTS user; 
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE user 
-  ( 
-     name         VARCHAR(50), 
-     screen_name  VARCHAR(50), 
-     sub_category VARCHAR(50), 
-     category     VARCHAR(50), 
-     location     VARCHAR(50), 
-     followers    BIGINT, 
-     following    BIGINT, 
-     PRIMARY KEY(screen_name), 
-     FOREIGN KEY(location) REFERENCES state(name) 
-  ); 
+CREATE TABLE user
+  (
+     name         VARCHAR(50),
+     screen_name  VARCHAR(50),
+     sub_category VARCHAR(50),
+     category     VARCHAR(50),
+     location     VARCHAR(50),
+     followers    BIGINT,
+     following    BIGINT,
+     PRIMARY KEY(screen_name),
+     FOREIGN KEY(location) REFERENCES state(name)
+  );
 
-DROP TABLE IF EXISTS tweet; 
+DROP TABLE IF EXISTS tweet;
 
-CREATE TABLE tweet 
-  ( 
-     id            BIGINT, 
-     created_at    DATE, 
-     day           VARCHAR(31), 
-     month         VARCHAR(12), 
-     year          BIGINT, 
-     text          VARCHAR(280), 
-     retweet_count BIGINT, 
-     retweeted     BIT, 
-     /* A bit is either 1, 0, or null. Good to use as a boolean. */ 
-     posting_user  VARCHAR(50), 
-     PRIMARY KEY(id), 
-     FOREIGN KEY(posting_user) REFERENCES user(screen_name) ON DELETE CASCADE 
-  ); 
+CREATE TABLE tweet
+  (
+     id            BIGINT,
+     created_at    DATE,
+     day           VARCHAR(31),
+     month         VARCHAR(12),
+     year          BIGINT,
+     text          VARCHAR(280),
+     retweet_count BIGINT,
+     retweeted     BOOL,
+     /* A bit is either 1, 0, or null. Good to use as a boolean. */
+     /* True or false can also be null - Dylan */
+     posting_user  VARCHAR(50),
+     PRIMARY KEY(id),
+     FOREIGN KEY(posting_user) REFERENCES user(screen_name) ON DELETE CASCADE
+  );
+CREATE TABLE IF EXISTS state
+(
+    name VARCHAR(2),
+    screen_name VARCHAR(50),
+
+    PRIMARY KEY(name),
+    FOREIGN KEY(screen_name) REFERENCES user(screen_name) ON DELETE CASCADE
+)
+CREATE TABLE IF EXISTS url
+    (
+        address VARCHAR(50),
+        tweet_id BIGINT
+
+        PRIMARY KEY(address),
+        FOREIGN KEY(tweet_id) REFERENCES tweet(id) ON DELETE CASCADE
+
+    )
+
+CREATE TABLE IF EXISTS hashtag
+(
+    name VARCHAR(50),
+    tweet_id BIGINT
+
+    PRIMARY KEY(name),
+    FOREIGN KEY(id) REFERENCES tweet(id) ON DELETE CASCADE
+)
+
+
