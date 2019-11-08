45e113d17ea2e35019fc247cc07f0fc2217af8d7
Sheets <smsheets@iastate.edu>
2019-11-07 22:46:17 -0600

    ok

--- CreateApplicationUsers.sql
+++ CreateApplicationUsers.sql
@@ -1,13 +1,17 @@
 CREATE DATABASE IF NOT EXISTS group6;
+
 USE group6;
+
 DROP 
   TABLE IF EXISTS appusers;
+  
 CREATE TABLE appusers (
   login_name VARCHAR(50), 
   upassword VARCHAR(50), 
   privilege VARCHAR(8), 
   PRIMARY KEY(login_name)
 );
+
 INSERT INTO appusers 
 VALUES 
   (
@@ -15,6 +19,7 @@ VALUES
     Sha1('!Group6!@cs363'), 
     'all'
   );
+  
 INSERT INTO appusers 
 VALUES 
   (
--- CreateDBUsers.sql
+++ CreateDBUsers.sql
@@ -2,11 +2,12 @@ CREATE DATABASE IF NOT EXISTS group6;
 use group6;
 
 CREATE USER IF NOT EXISTS 'cs363' @'%1' 
-IDENTIFIED WITH mysql_native_password BY 'cs363pak';
+	IDENTIFIED WITH mysql_native_password BY 'cs363pak';
 
 GRANT 
   view, 
   drop, 
   create, 
   insert, 
-  delete ON group6.* TO 'cs363' @'%1';
+  delete 
+	ON group6.* TO 'cs363' @'%1';
