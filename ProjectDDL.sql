756e2526c1a381eaabe0e365bdeea1ec06f347f2
Dylan Smith <dylan1999.smith@gmail.com>
2019-11-07 23:22:05 -0600

    fixed ddl

--- ProjectDDL.sql
+++ ProjectDDL.sql
@@ -53,4 +53,55 @@ CREATE TABLE mentioned
      PRIMARY KEY(uscreen_name, tid), 
      FOREIGN KEY(uscreen_name) REFERENCES user(screen_name), 
      FOREIGN KEY(tid) REFERENCES tweet(id) 
-  ); 
+  );
+  
+  CREATE TABLE url_used
+  (
+	address  	VARCHAR(50), 
+	tid 		BIGINT, 
+    PRIMARY KEY(address, tid),
+    FOREIGN KEY(address) REFERENCES url(address),
+    FOREIGN KEY(tid) REFERENCES tweet(id)
+  );
+  
+  CREATE TABLE tagged
+  (
+	name  		VARCHAR(50), 
+	tid 		BIGINT, 
+    PRIMARY KEY(name, tid),
+    FOREIGN KEY(name) REFERENCES hashtag(name),
+    FOREIGN KEY(tid) REFERENCES tweet(id)
+  );
+
+delimiter ||
+
+CREATE TRIGGER `load_hashtag` 
+BEFORE INSERT ON tagged FOR EACH ROW 
+BEGIN IF (SELECT count(*) FROM hashtag WHERE hashtag.name=NEW.hashtag) = 0 
+THEN INSERT INTO hashtag(name) VALUES(NEW.hashtag); 
+END IF;
+END;||
+
+CREATE TRIGGER `delete_hashtag` 
+BEFORE DELETE ON tagged FOR EACH ROW 
+BEGIN IF (SELECT count(*) FROM tagged WHERE tagged.hashtag=OLD.hashtag) <= 1 THEN
+
+DELETE FROM hashtag WHERE OLD.hashtag=hashtag.name; END IF;
+
+END;||
+
+CREATE TRIGGER `load_url` 
+BEFORE INSERT ON url_used FOR EACH ROW
+BEGIN IF (SELECT count(*) FROM url WHERE url.address=NEW.url) = 0 
+THEN INSERT INTO url(address) VALUES(NEW.url);
+END IF; 
+END;||
+
+CREATE TRIGGER `delete_url` 
+BEFORE DELETE ON `url_used` FOR EACH ROW 
+BEGIN IF (SELECT count(*) FROM url_used WHERE url_used.url=OLD.url) <= 1 
+THEN DELETE FROM url WHERE OLD.url=url.address; 
+END IF;
+END;||
+
+delimiter ;
