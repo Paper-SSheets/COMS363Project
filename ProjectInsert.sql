b69d40d67295309f0e464e39be65a489539093cb
andmarek <andmarek@outlook.com>
2019-11-07 22:47:26 -0600

    "Init for projectinsert"

---
+++ ProjectInsert.sql
@@ -0,0 +1,14 @@
+/* Essentially a template right here */
+LOAD DATA INFILE '/Users/andrewmarek/code/COMS363Project/dataCSV/user.csv'
+INSERT INTO TABLE user VALUES 
+FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+()
+
+
+INSERT INTO tweet VALUES () 
+INSERT INTO url VALUES ;
+INSERT INTO hashtag VALUES;
+INSERT INTO mentioned VALUES;
+
