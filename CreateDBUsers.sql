d8b7cd8fe7e488d7146faad88bebde346fb8b935
Sheets <smsheets@iastate.edu>
2019-11-07 22:36:20 -0600

    format CreateDBUsers.sql

--- CreateDBUsers.sql
+++ CreateDBUsers.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE if not exists group6;
 use group6;
 CREATE USER IF NOT EXISTS 'cs363'@'%1' IDENTIFIED WITH mysql_native_password BY 'cs363pak';
-GRANT view, drop, create, insert, delete ON group6.*TO'cs363'@'%1';
+GRANT view, drop, create, insert, 
+delete
+   ON group6.*TO'cs363'@'%1';
