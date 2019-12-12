3fd0ba87514103b95bb625c1c521e3ce7719fc61
andmarek <andmarek@outlook.com>
2019-12-12 13:26:53 -0600

    java now..?

--- ProjectInsert.sql
+++ ProjectInsert.sql
@@ -1,6 +1,6 @@
 USE group6;
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/user.csv'
+LOAD DATA INFILE './dataCSV/user.csv'
 INTO TABLE user
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '"'
@@ -9,7 +9,7 @@ IGNORE 1 LINES
 (screen_name, dname, sub_category, category, 
  state_name, num_followers, num_following);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dataCSV/tweets.csv'
+LOAD DATA INFILE './dataCSV/tweets.csv'
 INTO TABLE tweet
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '"'
@@ -18,7 +18,7 @@ IGNORE 1 LINES
 (tid, tweet_text, retweet_count, 
  retweeted, created_at, uscreen_name);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/uploads/dataCSV/urlused.csv'
+LOAD DATA INFILE './dataCSV/urlused.csv'
 INTO TABLE Url_used
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '"'
@@ -26,7 +26,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (tid, url);
  
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dataCSV/tagged.csv'
+LOAD DATA INFILE './dataCSV/tagged.csv'
 INTO TABLE tagged
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '"'
@@ -34,7 +34,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (tid, hashtag);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dataCSV/mentioned.csv'
+LOAD DATA INFILE './dataCSV/mentioned.csv'
 INTO TABLE mentioned
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '"'
